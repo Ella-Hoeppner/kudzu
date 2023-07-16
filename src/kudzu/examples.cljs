85958c339de5b94de078f2c78b68529019630fcb
Ella Hoeppner <ellahoeppner@gmail.com>
2023-07-16 10:47:15 -0400

    changing example comment formatting

--- src/kudzu/examples.cljs
+++ src/kudzu/examples.cljs
@@ -24,7 +24,7 @@
   (kudzu->glsl
    '{:main ((=vec3 color)
 
-            ;conditional blocks are denoted by a keyword
+            ; Conditional blocks are denoted by a keyword.
             (:if false
                  (= color (vec3 1)))
             (:else-if false
@@ -32,7 +32,7 @@
             (:else
              (= color (vec3 0)))
 
-            ;ternaries are also supported
+            ; Ternaries are also supported.
             (= color (if true
                        (vec3 1)
                        (vec3 0))))}))
@@ -41,33 +41,39 @@
   (kudzu->glsl
    '{:main
      ((=vec3 color)
-      ;there are two ways to express for-loops
+      ; There are two ways to express for-loops in kudzu.
 
-      ;the first is with syntax similar to Clojure's for fn
-      ;a binding vector that takes a name and an end value    
+      ; The first is with syntax similar to Clojure's "for"
+      ; a binding vector that takes a name and an end value.
       (:for [x 10]
             (* color x))
-      ;you can also add an initial value
+      
+      ; You can also add an initial value,
       (:for [y 0 10]
             (* color y))
-      ;and a step value
+      ; and a step value.
       (:for [z 0 10 2]
             (* color z))
 
-      ;the second is more C-like, when fine-grained control is needed
+      ; The second syntax is more C-like, for when fine-grained
+      ; control is needed.
       (:for (=uint i 0) (<= i 0xFF) (>> i 3)
             (*= color y))
 
-      ;while loops are similar, taking a single expression
+      ; while loops are similar, taking a single expression.
+      (:while (< x 0.5)
+              (+= x 0.1))
       (:while true
               (= color (rand))
               "break")
 
-      ;kudzu also supports naked blocks
+      ; Kudzu also supports naked blocks, for local bindings
+      ; that fall out of scope once the block is finished.
       (:block
        (=float unreachable 0))
       
-      ;this throws an error
+      ; This would throw an error, as "unreachable" is no
+      ; longer in scope.
       (++ unreachable))}))
 
 (def threading-example
@@ -76,8 +82,7 @@
      :main
      ((=vec2 pos (/ gl_FragCoord resolution))
 
-      ;Most of Clojure's threading macros are supported 
-      ;and can be used within one another
+      ; Most of Clojure's threading macros are supported. 
       (= pos (-> pos
                  (* 100)
                  noise))
@@ -85,65 +90,65 @@
                   (texture tex)
                   .rgb
                   vec3))
+      (= fragColor (as-> pos p
+                     (texture tex p)
+                     (.rgb p)
+                     (mod p (vec3 0.5))
+                     (vec4 p 1)))
 
-      ;though in the case of modifying a variable, there are modifier versions
+      ; In the case of modifying a variable, there are modifier versions.
       (=-> pos
            (* 100)
            noise)
       (=->> pos
             (texture tex)
             .rgb
-            vec3)
-
-      ;thread-as 
-      (= fragColor (as-> pos p
-                     (texture tex p)
-                     (.rgb p)
-                     (mod p (vec3 0.5))
-                     (vec4 p 1))))}))
+            vec3))}))
 
 (def struct-example 
-  ;structs are defined in a hash-map
-  ;where the key is the name of the struct
-  ;and the value is a vector of the struct's fields and their types
+  ; Structs are defined in a hash-map, where the key is the name of
+  ; the struct, and the value is a vector of the struct's fields and
+  ; their types.
   (kudzu->glsl
    '{:structs
      {Ray
       [pos vec3
        dir vec3]
 
-      ;structs can reference other structs
+      ; Structs can reference other structs,
       Record
       [pos vec3
        material Material]
-      ;and can be added in any order as they're sorted at compile time
+      
+      ; and can be defined in any order, as they're sorted at compile time.
       Material
       [albedo vec3]}
+     :functions
+     ; Structs can be passed to functions lke any other type.
+     {ray-at
+      (Ray
+       [ray Ray
+        t float]
+       (Ray (+ ray.pos
+               (* ray.dir t))
+            ray.dir))}
      :main
-     (;structs type will always be '=Name'
-      ;they can be constructed by calling the (capitalized) name
-      ;with the fields as arguments
+     (; structs are declared with the "=Name" syntax, like other types.
+      ; They can be constructed by calling the name with values for 
+      ; their fields as arguments. Keep in mind that struct names
+      ; are case-sensitive.
       (=Ray ray (Ray (vec3 0) (vec3 0.5)))
 
-      ;fields can be accesed with dot syntax
+      ; Fields can be accesed with dot syntax,
       (=vec3 pos ray.pos)
       (=vec3 dir ray.dir)
 
-      ;even when the field is a struct
-      (=vec3 color record.material.albedo)
-      (=vec3 color (.albedo (.material record)))
-
-      ;dot syntax can also be used in place of a function call
       (=vec3 pos (.pos ray))
-      (=vec3 dir (.dir ray)))
-     :functions 
-     ;structs are passed to functions lke any other type
-     {ray-at 
-      (Ray
-       [ray Ray t float]
-       (Ray (+ ray.pos
-               (* ray.dir t))
-            ray.dir))}}))
+      (=vec3 dir (.dir ray))
+
+      ; including when the field is itself a struct.
+      (=vec3 color record.material.albedo)
+      (=vec3 color (.albedo (.material record))))}))
 
 (def hollow-demos-link
   "https://github.com/Ella-Hoeppner/hollow/tree/main/src/hollow/demos")
